c7d70e9768e08825a6ea69fe9e99e852822fa9b8
dennis <dennis.olimpio0794@gmail.com>
2020-05-27 15:23:38 +0200

    ho aggiunto else if

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ for ( var i = 1 ; i <= 100; i++) {
 
   // CONDIZIONE:
   // Se il numero diviso 3 da resto 0,  &
-  // Se il numero divo 5 da resto diverso da zero
+  // Se il numero diviso 5 da resto diverso da zero
   // Scrivo Fizz.
 
   if  ((numeri % 3 === 0 ) && !( numeri % 5 == 0 )) {
@@ -28,27 +28,24 @@ for ( var i = 1 ; i <= 100; i++) {
   // Scrivo Buzz.
 
 
-  if (( numeri % 5 == 0 ) && !(numeri % 3 === 0 ) ) {
+  else if  (( numeri % 5 == 0 ) && !(numeri % 3 === 0 ) ) {
     console.log( ' Buzz.');
-
-
-
   }
 
   // CONDIZIONE:
   // Se il numero diviso 3 da resto 0,  &
-  // Se il numero divo 5 da resto  zero
+  // Se il numero diviso 5 da resto  zero
   // Scrivo FizzBuzz.
 
-  if  ((numeri % 3 == 0) && (numeri % 5 == 0)) {
+  else if  ((numeri % 3 == 0) && (numeri % 5 == 0)) {
     console.log(' FizzBuzz');
 
   }
 
   // CONDIZIONE:
-  // Se entrambi i valori non rispondono alle condizioni precedenti allora stampa il numero
+  // Se entrambi i valori non rispondono alle condizioni precedenti, allora stampa il numero
+
   else {
-  if (!(numeri % 3 == 0) && !(numeri % 5 == 0))
     console.log(numeri);
   }
 
