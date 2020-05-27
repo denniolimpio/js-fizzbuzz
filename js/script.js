5c5dd549e07dc4cf7003e33ac3b8f49b25c3987b
dennis <dennis.olimpio0794@gmail.com>
2020-05-27 16:42:07 +0200

    ho aggiunto la versione con While

--- js/script.js
+++ js/script.js
@@ -4,58 +4,99 @@
 // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
 
+
+// versione FOR
+// //variabili html
+// var outputTre = document.getElementById('outputMult3');
+// var outputCinque = document.getElementById('outputMult5');
+// var outputMult = document.getElementById('outputMult')
+//
+// // Stampo i numeri da 1 a 100
+//
+// for ( var i = 1 ; i <= 100; i++) {
+  // var numeri = i;
+  //
+  // // CONDIZIONE:
+  // // Se il numero diviso 3 da resto 0,  &
+  // // Se il numero diviso 5 da resto diverso da zero
+  // // Scrivo Fizz.
+  //
+  // if  ((numeri % 3 === 0 ) && !( numeri % 5 == 0 )) {
+  //   outputTre.innerHTML += numeri + " <li> " + " Fizz. " + " </li> " + " ---  ";
+  //
+  //   console.log( " Fizz. ");
+  // }
+  //
+  // // CONDIZIONE:
+  // // Se il numero diviso 5 da resto 0,  &
+  // // Se il numero diviso 3 da resto diverso da zero
+  // // Scrivo Buzz.
+  //
+  //
+  // else if  (( numeri % 5 == 0 ) && !(numeri % 3 === 0 ) ) {
+  //   console.log( ' Buzz.');
+  //   outputCinque.innerHTML += numeri + " <li> " + " Buzz. " + " </li> " + " --- ";
+  //
+  // }
+  //
+  // // CONDIZIONE:
+  // // Se il numero diviso 3 da resto 0,  &
+  // // Se il numero diviso 5 da resto  zero
+  // // Scrivo FizzBuzz.
+  //
+  // else if  ((numeri % 3 == 0) && (numeri % 5 == 0)) {
+  //   outputMult.innerHTML += numeri + " <li> " + " FizzBuzz. " + " </li> " + " --- ";
+  //   console.log(' FizzBuzz');
+  //
+  //
+  // }
+  //
+  // // CONDIZIONE:
+  // // Se entrambi i valori non rispondono alle condizioni precedenti, allora stampa il numero
+  //
+  // else {
+  //   console.log(numeri);
+  // }
+//
+// }
+
+// Versione While
+
 //variabili html
 var outputTre = document.getElementById('outputMult3');
 var outputCinque = document.getElementById('outputMult5');
 var outputMult = document.getElementById('outputMult')
 
-// Stampo i numeri da 1 a 100
+// stampo i numeri da 1 a 100
 
-for ( var i = 1 ; i <= 100; i++) {
+var i = 1;
+while ( i <= 100) {
+
   var numeri = i;
 
-  // CONDIZIONE:
-  // Se il numero diviso 3 da resto 0,  &
-  // Se il numero diviso 5 da resto diverso da zero
-  // Scrivo Fizz.
-
   if  ((numeri % 3 === 0 ) && !( numeri % 5 == 0 )) {
-    outputTre.innerHTML += numeri + " <li> " + " Fizz. " + " </li> " + "  ";
+    outputTre.innerHTML += numeri + " <li> " + " Fizz. " + " </li> " + " ---  ";
 
     console.log( " Fizz. ");
   }
 
-  // CONDIZIONE:
-  // Se il numero diviso 5 da resto 0,  &
-  // Se il numero diviso 3 da resto diverso da zero
-  // Scrivo Buzz.
-
 
   else if  (( numeri % 5 == 0 ) && !(numeri % 3 === 0 ) ) {
     console.log( ' Buzz.');
-    outputCinque.innerHTML += numeri + " <li> " + " Buzz. " + " </li> " + "  ";
+    outputCinque.innerHTML += numeri + " <li> " + " Buzz. " + " </li> " + " --- ";
 
   }
 
-  // CONDIZIONE:
-  // Se il numero diviso 3 da resto 0,  &
-  // Se il numero diviso 5 da resto  zero
-  // Scrivo FizzBuzz.
-
-  else if  ((numeri % 3 == 0) && (numeri % 5 == 0)) {
-
-
-    outputMult.innerHTML += numeri + " <li> " + " FizzBuzz. " + " </li> " + "  ";
+  else if  (numeri % 15 == 0) {
+    outputMult.innerHTML += numeri + " <li> " + " FizzBuzz. " + " </li> " + " --- ";
     console.log(' FizzBuzz');
 
 
   }
 
-  // CONDIZIONE:
-  // Se entrambi i valori non rispondono alle condizioni precedenti, allora stampa il numero
-
   else {
     console.log(numeri);
   }
-
+  //interrompo il ciclo
+  i++;
 }
